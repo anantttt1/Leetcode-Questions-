cfdaffc9bc0b57deeb8c4425d98755353c806633
Anant goyal <245011933+anantttt1@users.noreply.github.com>
2026-08-20 08:26:10 +0530

    Time: 19 ms (11.55%), Space: 42.4 MB (79.5%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x<2) return x;
+        long i=1;
+        while(i*i<=x){
+            i++;
+        }
+        return (int)(i-1);
+    }
+}
+
+
+
+
+
+
+
+
+
